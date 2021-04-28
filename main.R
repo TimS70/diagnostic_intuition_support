2cfeb5ab2c563359103bd9458b1df419fc6d5c9f
Tim <timschngns@aol.com>
2021-04-28 11:52:00 +0200

    Main description

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: User
+# Title     : Diagnostic Intuition Support
+# Objective : Help medical practicioners interpreting diagnostic test outcomes
+# Created by: Tim Schneegans (tim.schneegans@simplyrational.de)
 # Created on: 28.04.2021
 
 print('Hello')
+

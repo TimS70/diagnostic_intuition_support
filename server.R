ae52877fc7d3706c878c601cbea3a64824f3a34c
Tim <timschngns@aol.com>
2021-04-29 15:53:27 +0200

    Take the first test from the list, when Hersteller changes

--- server.R
+++ server.R
@@ -9,39 +9,49 @@ server <- function(input, output, session) {
 
     data <- load_test_data(file=file.path('tests', 'antigentests.csv'))
 
-    observe({
-        # We'll use these multiple times, so use short var names for
-        # convenience.
-        hersteller <- input$hersteller
-        test <- input$test
+    # Take the first test from the list, when Hersteller changes
+    observeEvent(input$hersteller, {
+        print('Hersteller event')
 
         hersteller_tests <- reactive({
             data %>% filter(hersteller == input$hersteller)})
 
+        proposed_test <- reactive({data %>%
+            filter(hersteller == input$hersteller) %>%
+            slice(1) %>%
+            dplyr::pull(handelsname)})
+
+            updateSelectInput(session, inputId="test",
+                  choices = as.list(hersteller_tests()$handelsname),
+                  selected = proposed_test())
+        })
+
+    observe({
         updateTextInput(session, inputId="test",
                         label = paste0('All tests available by ', input$hersteller),
                         value = paste("New text"))
 
-        selected_test <- reactive({
-            data %>% filter(handelsname == test)})
+        hersteller_tests <- reactive({
+            data %>% filter(hersteller == input$hersteller)})
 
         updateSelectInput(session, inputId="test",
-              choices = as.list(hersteller_tests()$handelsname),
-              selected = input$test)
+                          choices = as.list(hersteller_tests()$handelsname),
+                          selected = input$test)
+
+        selected_test <- reactive({
+            data %>% filter(handelsname == input$test)})
 
         updateNumericInput(session, inputId="sensitivity",
-              label = paste0("Sensitivity of ", test, 'in %'),
+              label = paste0('Sensitivity'),
               value = paste(selected_test()$sensitivity))
 
         updateNumericInput(session, inputId="specifity",
-              label = paste0("Specifity of ", test, 'in %'),
+              label = paste0('Specifity'),
               value = paste(selected_test()$specifity))
 
         # Plot
         prevalence_data <- reactive({
             get_prevalence_data(test_data=selected_test())})
-
-        print(head(prevalence_data()))
 
         output$plot <- renderPlot({
             p <- ggplot(data=prevalence_data(), aes(x=prevalence)) +

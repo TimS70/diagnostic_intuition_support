4f58d2338f1ae5a57fbd0bfeeb765613fb70eab2
Tim <timschngns@aol.com>
2021-05-03 16:55:02 +0200

    Cut the clutter on About page

--- txt_content/about.R
+++ txt_content/about.R
@@ -1,40 +1,40 @@
-about_us <- paste(
-    'We are ',
-    '<a href="https://www.simplyrational.de/">Simply Rational GmbH - ',
-    'the Institute for Decisions</a>',
-    '<br><br>',
-    '<p>We are a Spin-Off from the Research Group for Adaptive Behavior and Cognition under ',
-    'the supervision of Prof. Gerd Gigerenzer at the ',
-    'Max Planck Institute for Human Development. ',
-    'Our team comprises trained Behavioral and Data Scientists as well as ',
-    'experienced partners from diverse business areas. ',
-    'Our mission is to to improve clinical decision processes and empower ',
-    'health care workers to make best use of ',
-    'diagnostic tools, their expertise and collaboration in the team. ',
-    '</p><br>',
-    sep='')
+about_us <- paste('
+    We are
+    <a href="https://www.simplyrational.de/">Simply Rational GmbH -
+    the Institute for Decisions</a>
+    <br><br>
+    <p>We are a Spin-Off from the Research Group for Adaptive Behavior and Cognition under
+    the supervision of Prof. Gerd Gigerenzer at the
+    Max Planck Institute for Human Development.
+    Our team comprises trained Behavioral and Data Scientists as well as
+    experienced partners from diverse business areas.
+    Our mission is to to improve clinical decision processes and empower
+    health care workers to make best use of
+    diagnostic tools, their expertise and collaboration in the team.
+    </p><br>
+    ', sep='')
 
-about_the_tool <- paste(
-    'About this tool:',
-    '<p>The Diagnostic Intuition Support Tool serves the purpose of giving a ',
-    'basic orientation for interpreting COVID-19 Antigen Tests. ',
-    'We do not give any warranty that the information provided on the ',
-    'website will be accurate or complete at any particular time or at all.',
-    'The information about the presented tests are directly obtained from the ',
-    'from the Federal Institute for Drugs and Medical Devices. ',
-    'The sensitivity and specifity information originates from the manufacturer. ',
-    'The most recend dataset can be found ',
-    '<a href="https://www.bfarm.de/DE/Medizinprodukte/Antigentests/_node.html;jsessionid=4F3D066A1BAB04CBFD7B0C7ACE1BA6B2.1_cid506">',
-    'here</a>.</p><br>',
-    sep='')
+about_the_tool <- paste('
+    About this tool:
+    <p>The Diagnostic Intuition Support Tool serves the purpose of giving a
+    basic orientation for interpreting COVID-19 Antigen Tests.
+    We do not give any warranty that the information provided on the
+    website will be accurate or complete at any particular time or at all.
+    The information about the presented tests are directly obtained from the
+    from the Federal Institute for Drugs and Medical Devices.
+    The sensitivity and specifity information originates from the manufacturer.
+    The most recend dataset can be found
+    <a href="https://www.bfarm.de/DE/Medizinprodukte/Antigentests/_node.html;jsessionid=4F3D066A1BAB04CBFD7B0C7ACE1BA6B2.1_cid506">
+    here</a>.</p><br>
+    ', sep='')
 
 contact_us <- paste(
     ' ',
-            'Contact us',
-            'kontakt@simplyrational.de',
-            ' ',
-            'Simply Rational GmbH',
-            'Eberhard-Roters-Platz 7a',
-            'DE-10965 Berlin',
-            sep="<br/>")
+    'Contact us',
+    'kontakt@simplyrational.de',
+    ' ',
+    'Simply Rational GmbH',
+    'Eberhard-Roters-Platz 7a',
+    'DE-10965 Berlin',
+    sep="<br/>")
 

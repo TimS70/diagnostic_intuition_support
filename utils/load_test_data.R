76330f7d8191994d9f1fa9656a3d6f4c607333de
Tim <timschngns@aol.com>
2021-05-17 15:32:58 +0200

    Remove comments

--- utils/load_test_data.R
+++ utils/load_test_data.R
@@ -73,18 +73,9 @@ clean_test_data <- function(data) {
 }
 
 load_test_data <- function(file) {
-#     link <- 'https://antigentest.bfarm.de/ords/wwv_flow.ajax?p_flow_id=110&p_flow_step_id=100&p_instance=15359458084502&p_debug=&p_json=%7B%22regions%22%3A%5B%7B%22id%22%3A%2288500488737603332%22%2C%22reportId%22%3A%2288808822859644913%22%2C%22ajaxIdentifier%22%3A%22EMuu7CyLWa5dWZgdfpQ6lI1yxyFfdU29Lpo1ZMnnZWc%22%2C%22downloadFileId%22%3A%22250415674117865674%22%7D%5D%2C%22salt%22%3A%2277076691586332798101117057462003301300%22%7D'
-#     link_2 <- 'https://antigentest.bfarm.de/ords/f?p=110:100:14588237856972:::::&tz=2:00'
-# # example <- EN.ATM.CO2E.PC
-# # NY.GDP.PCAP.CD
-#     test <- fread(link_2)
-#     test
     data <- read.csv(file=file, sep = ';', dec=',',
                      fileEncoding='windows-1252')
     data <- clean_test_data(data = data)
 
     return(data)
 }
-
-# dir.create(path='data')
-# write.csv(data, file.path('data', "test_data.csv"),fileEncoding = 'UTF-8')

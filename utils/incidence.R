87e68473a12230ddb9551ef0d1d6fbed8bd1baf7
Tim Schneegans <tim.schneegans@simplyrational.de>
2021-05-26 08:29:38 +0200

    Fixed incidence data (Have to check every day)

--- utils/incidence.R
+++ utils/incidence.R
@@ -26,21 +26,22 @@ region_names <- function() {
 
 region_incidence_data <- function() {
     url <- 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab.xlsx?__blob=publicationFile'
-    data_bundesland <- read.xlsx(url, rows=3:22) %>%
-        clean_names() %>%
-        rename(region = bundesland,
-               incidence = x7_tage_inzidenz) %>%
-        dplyr::select(region, incidence)
+    data_bundesland <- read.xlsx(url, rows=3:22, sheet=3, colNames=TRUE, 
+                                 detectDates=TRUE) 
+    data_bundesland <- data_bundesland[,c(1, ncol(data_bundesland))]
+    names(data_bundesland) <- c('region', 'incidence')
 
     data_bundesland[data_bundesland$'region'=='Gesamt', 'region'] <- 'Deutschland'
-
-    data_landkreis <- read.xlsx(url, rows=3:1000, colNames=TRUE, sheet=2) %>%
-        clean_names()
-    data_landkreis$landkreis_nr <- with(data_landkreis,
-                                        paste0(landkreis, ' (', lknr, ')'))
-    data_landkreis <- data_landkreis %>%
-        rename(region=landkreis_nr, incidence=inzidenz) %>%
-        dplyr::select(region, incidence)
+    
+    data_landkreis <- read.xlsx(url, rows=3:22, cols=2:1000, sheet=5, colNames=TRUE, 
+                                detectDates=TRUE) 
+    
+    data_landkreis <- data_landkreis[,c(1:2, ncol(data_landkreis))]
+    names(data_landkreis) <- c('LK', 'LKNR', 'incidence')
+    
+    data_landkreis$region <- with(data_landkreis, paste0(LK, ' (', LKNR, ')'))
+    
+    data_landkreis <- data_landkreis %>% dplyr::select(region, incidence)
 
     data <- rbind(data_bundesland,
                   data_landkreis)

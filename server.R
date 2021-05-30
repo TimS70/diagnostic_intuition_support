d1b659cd6281707bb762afee7d4ccfb80486f085
Tim <schneeganstim@gmail.com>
2021-05-30 23:56:56 +0200

    Update test dataset

--- server.R
+++ server.R
@@ -15,7 +15,7 @@ source(file='visualize/generate_plot.R')
 
 server <- function(input, output, session) {
 
-    data <- load_test_data(file= file.path('data', 'antigentests.csv'))
+    data <- load_test_data(file=file.path('data', 'antigentests.csv'))
     data_region <- region_incidence_data()
 
     introjs(session, options = list("nextLabel"="Weiter",

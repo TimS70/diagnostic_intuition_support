fd568a319a1b8ee8174bde075f6d777d59717e0f
Tim <schneeganstim@gmail.com>
2021-06-10 16:14:48 +0200

    Added user statistics

---
+++ user_stats/main.R
@@ -0,0 +1,90 @@
+root <- "C:/Users/TimSchneegans/Documents"
+setwd(file.path(root, 'github', 'diagnostic_intuition_support', 'user_stats'))
+
+library(tidyverse)
+library(anytime)
+library(scales)
+library(lubridate)
+
+# http://docs.rstudio.com/shinyapps.io/metrics.html#ApplicationMetrics
+# data_usage <- rsconnect::showUsage(
+#     account = 'simplyrational', 
+#     appName = 'diagnostic_intuition_support', 
+#     usageType = 'hours', 
+#     from=as.numeric(as.POSIXct("2021-05-31", format="%Y-%m-%d")),
+#     until=as.numeric(as.POSIXct("2021-06-10", format="%Y-%m-%d"))
+# )
+
+data_connect <- rsconnect::showMetrics(
+    metricSeries="container_status",
+    metricNames=c("connect_count", 'connect_procs'),
+    appName="diagnostic_intuition_support",
+    server="shinyapps.io",
+    from=as.numeric(as.POSIXct("2021-05-31", format="%Y-%m-%d")),
+    until=as.numeric(as.POSIXct("2021-06-10", format="%Y-%m-%d"))
+) 
+names(data_connect) = c('connect_count', 
+                        'metric_series', 
+                        'connect_procs', 
+                        'timestamp')
+
+
+data <- data_connect %>%
+    mutate( 
+        # Clean variables
+        connect_count = as.numeric(connect_count),
+        connect_procs = as.numeric(connect_procs),
+        timestamp = as.numeric(as.character(timestamp))
+   ) %>%
+#    filter(connect_count == 1 & connect_procs == 1) %>% 
+    mutate(
+        # Additional variables
+        date=as.Date(as.POSIXct(timestamp, origin="1970-01-01")),
+        n_count=cumsum(connect_count),
+        n_procs=cumsum(connect_procs),
+        new_connect=case_when(
+            connect_count>lag(connect_count, 1) ~ 
+                connect_count-lag(connect_count, 1),
+            TRUE ~ 0),
+        n_connect=cumsum(new_connect) # approximate
+    ) %>% 
+    filter(n_count>0)
+
+grouped_new_connect <- data %>% 
+    group_by(date) %>%
+    dplyr::summarise(
+        n=sum(new_connect),
+        .groups='keep'
+    )
+
+ggplot(data=grouped_new_connect, aes(x=date, y=n)) +
+    geom_bar(stat="identity") + 
+    theme_minimal() +
+    scale_x_date(
+        labels = date_format("%m-%Y"),
+        breaks = grouped_new_connect$date) + 
+    theme(
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        panel.grid.minor.x=element_blank(),
+        panel.grid.major.x=element_blank()
+    ) +
+    ggtitle('New Connections by Day')
+
+ggsave(
+    filename = file.path('results', 'connections.png'),
+    width=5.5, height=5
+)
+
+data_cum_connections <- data %>%  
+    select(n_connect, date) %>% 
+    gather(key="key", value="value", -date)
+
+ggplot(data_cum_connections) +
+    labs(title="Cumulative Connections", x="", y="") +
+    geom_line(aes(x=date, y=value, colour=key)) +
+    facet_wrap(~key)
+
+ggsave(
+    filename = file.path('results', 'connections_cumulative.png'),
+    width=5.5, height=5
+)
